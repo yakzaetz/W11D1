18490fcdc1b9e5bfe7c8dbc8ad665498c606800e
yakzaetz <yakzaetz@gmail.com>
2022-02-14 15:14:07 -0500

    attempted nested map of the grid

--- minesweeper/components/board.jsx
+++ minesweeper/components/board.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 class Board extends React.Component {
     
-    render() {
-        debugger 
-        return null;
+    render() { 
+        return (
+            <div {this.props.board.grid.map((row, idx) => 
+                <div {row}/>
+                <div {row.map((tile, idx)) => 
+                    <div tile/>
+                }/>
+            )} />
+        )
     }
 }
 
