902d14dc29ba30ec42239141d4d913f428372f2b
yakzaetz <yakzaetz@gmail.com>
2022-02-14 12:17:11 -0500

    alias completed and board props completed

--- minesweeper/components/game.jsx
+++ minesweeper/components/game.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import Minesweeper from "../minesweeper";
+import * as Minesweeper from "../minesweeper";
 
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {board: new Minesweeper.Board()}; 
+        this.updateGame = this.updateGame.bind(this);
+    }
+
+    updateGame(){
+
+    }
+
+    render(){
+        return(
+            <div>
+                <Board board={this.state.board} updateGame={this.updateGame}/>
+            </div>
+        )
     }
 }
