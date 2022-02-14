ceb40d1432179e96d2a47ef440de69f81124797e
sean <midruss@yahoo.com>
2022-02-14 15:32:26 -0500

    Explored nested mapping and variables for board render

--- minesweeper/components/board.jsx
+++ minesweeper/components/board.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
 class Board extends React.Component {
-    
-    render() { 
-        return (
-            <div {this.props.board.grid.map((row, idx) => 
-                <div {row}/>
-                <div {row.map((tile, idx)) => 
-                    <div tile/>
-                }/>
-            )} />
-        )
+    const realTiles = this.props.board.grid
+
+    render() {
+        debugger 
+        return null;
     }
-}
+    
+//     render() { 
+//         return (
+//             <div {this.props.board.grid.map((row, idx) => 
+//                 <div {row}/>
+//                 <div {row.map((tile, idx)) => 
+//                     <div tile/>
+//                 }/>
+//             )} />
+//         )
+//     }
+// }
 
 export default Board;
